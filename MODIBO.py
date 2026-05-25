765794bd13129506329b7cdf492d3351215a7d43
Kenzus <bernes.ndoussy@epitech.eu>
2026-05-25 20:30:19 +0200

    [ADD] on_ready fucntion [MODIBO.py]

--- MODIBO.py
+++ MODIBO.py
@@ -6,12 +6,25 @@ from discord.ext import commands
 load_dotenv()
 
 ##INITIALIASTION DU BOT
+intents = discord.Intents.default()
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
+client = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print("MODIBO est réveiller !!")
+    print(f"Vous êtes connecter entant que: {client.user}")
+    try:
+        syncro = await bot.tree.sync()
+        print(f"Commandes slash synchronisées: {len(syncro)}")
+    except Exception as error:
+        print(error)
 
 @bot.tree.command(name="play_music", description="joue la musique demander par l'utilisateur")
 async def play_music(interraction: discord.Interaction):
     await interraction.response.launch_activity()
 
-##LANCEMENT DU BOT
+0##LANCEMENT DU BOT
 bot.run(os.getenv('DISCORD_TOKEN'))
